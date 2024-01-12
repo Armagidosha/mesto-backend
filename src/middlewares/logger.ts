0b5d158faada8c75611b4cdf465dd53d25fef8f5
Armagidoshka <armgidosha@list.ru>
2024-01-12 17:17:20 +0300

    change logs dir

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,17 +1,18 @@
 import winston from 'winston';
 import expressWinston from 'express-winston';
 
+// Request logger
 export const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: 'request.log', dirname: './logs', maxFiles: 14 }),
   ],
   format: winston.format.json(),
 });
 
-// логер ошибок
+// Error logger
 export const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: 'error.log', dirname: './logs', maxFiles: 14 }),
   ],
   format: winston.format.json(),
 });
